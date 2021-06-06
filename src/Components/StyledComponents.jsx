c7c3680cbdd07a24242e022d771534d3323fb2e9
ralph <wealthralph@gmail.com>
2021-06-06 03:10:45 +0100

    single room page completed

--- src/Components/StyledComponents.jsx
+++ src/Components/StyledComponents.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
-
+import defaultImg  from '../images/room-3.jpeg'
 
 
 const StyledHero = styled.header`
 min-height : 60vh;
-backgroung: url(${props => props.img}) center/cover no-repeat;
+background: url(${props =>(props.img ? props.img : defaultImg) }) center/cover no-repeat;
 display: flex;
 align-items: center ;
 justify-content: center ;
--- src/Pages/SingleRooms.jsx
+++ src/Pages/SingleRooms.jsx
@@ -5,6 +5,7 @@ import Banner from '../Components/Banner'
 import {Link} from 'react-router-dom'
 import {RoomContext} from '../Context'
 import StyledHero from '../Components/StyledComponents'
+import '../App.css'
 
 export default class SingleRooms extends Component {
     constructor(props) {
@@ -12,15 +13,15 @@ export default class SingleRooms extends Component {
         console.log();
         this.state = {
             slug: this.props.match.params.slug,
-            defaultBcg
+            img: defaultBcg
         };
     }
 
     static contextType = RoomContext;
     render() {
 
-        const {getRoom} = this.context;
-            const room  = getRoom(this.state.slug);
+        const {getRooms} = this.context;
+            const room  = getRooms(this.state.slug);
 
             if (!room) {
                 return  <div className = 'error'>
@@ -32,13 +33,51 @@ export default class SingleRooms extends Component {
             const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
 
         return (
-            <StyledHero img={images[0]}>
+            <React.Fragment>
+            <StyledHero img={images[0] || this.state.img}>
                 <Banner title={`${name} room`}  >
                     <Link to='/rooms' className ='btn-primary' >
                         Back to rooms 
                     </Link>
                 </Banner>
             </StyledHero>
+
+            <section className="single-room">
+                <div className="single-room-images">
+                    {images.map((item, index) => {
+                       return <img key={index} src={item} alt={name} />
+                    })}
+                </div>
+                <div className="single-room-info">
+                    <article classname='desc'>
+                        <h3>Details</h3>
+                        <p>{description}</p>
+                    </article>
+
+                    <article className="info">
+                        <h3>Info</h3>
+                        <h6>Price: ${price} </h6>
+                        <h6>Size : ${size} </h6>
+                        <h6>max capacity :{capacity > 1 ? `${capacity} people` : `${capacity} Person`} </h6>
+                        <h6>{pets ? 'pets are allowed' : "no pets allowed" }</h6>
+                        <h6>{breakfast && 'free breakfast included' }</h6>
+
+                    </article>
+                </div>
+
+
+            </section>
+
+            <section className="room-extras">
+                <h6>Extras</h6>
+                <ul className="extras">
+                    {extras.map((item, index) => {
+                        return <li key={index}  >- {item}</li>
+                    })}
+                </ul>
+            </section>
+            </React.Fragment>
+            
         )
     }
 }
